5aceb9ad2845d8f31464231255ad15f7f0caf87c
dmtrii-kg <dmitryi.kaganov@gmail.com>
2018-10-28 14:38:36 +0300

    version 0.2

--- 1606-1/kaganov da/main.cpp
+++ 1606-1/kaganov da/main.cpp
@@ -1,42 +1,40 @@
 #include <iostream>
+#include <iomanip>
 #include <random>
 #include <ctime>
 #include "mpi.h"
 using namespace std;
 
-char* FillStr(int n)
+void FillStr(char *str, int n)
 {
 	int j;
-	char* str;
-	str = new char[n];
-	for(int i = 0; i < n; i++)
-		str[i] = rand()%25 + 97;
-	for(int i = 0; i < n / 10; i++)
+	for (int i = 0; i < n; i++)
+		str[i] = rand() % 25 + 97;
+	str[n] = '\0';
+	for (int i = 0; i < n / 10; i++)
 	{
-		j = rand() % n;
+		j = 1 + rand() % (n - 1);
 		str[j] = '.';
 	}
-	return str;
 }
 
 bool searchDot(char c)
 {
-	switch(c)
+	switch (c)
 	{
-		case '.':
-		case '!':
-		case '?':
-			return true;
-		default:
-			return false;
+	case '.':
+	case '!':
+	case '?':
+		return true;
+	default:
+		return false;
 	}
 }
 
 int main(int argc, char *argv[])
 {
 	srand(time(0));
-	int flag;
-	int n = 17;						// string lenght
+	int n = 1000000;				    // string lenght
 	int counter = 0;					// number of sentences
 	int my_counter = 0;
 	int remainderDiv = 0;
@@ -44,88 +42,113 @@ int main(int argc, char *argv[])
 	double ls_time, le_time, start_time, end_time;
 
 	// random fill of string
-	
-	// cout << "Введите длину строки: "; cin >> n;
-	char* str;
-	str = new char[n];
-	str = FillStr(n);
-	cout << "Полученная строка: " << str << endl;
+    int* CounterVec;
+    char* str;
+    char* my_str;
+	str = new char[n + 1];
+	FillStr(str, n);
 
 	MPI_Status status;
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &ProcNum);
 	MPI_Comm_rank(MPI_COMM_WORLD, &ProcRank);
-	
-	// linear block
-	if(ProcRank == 0)
+
+	if (ProcRank == 0)
 	{
+    // linear block
+        // cout << "\nEnter the string: "; cin >> setw(n) >> str;
+		cout << "\nReceived string: " << str << endl;
+
 		ls_time = MPI_Wtime();
-		for(int i = 1; i < n; i++)
+		for (int i = 1; i < n; i++)
 		{
-			if(searchDot(str[i]))
+			if (searchDot(str[i]))
 			{
-				if(!searchDot(str[i - 1]))
+				if (!searchDot(str[i - 1]))
 				{
 					counter++;
 				}
 			}
 		}
 
-	// linear result
+    // linear result
 		le_time = MPI_Wtime();
 		cout << "\n LINEAR \n";
-		cout << "В введённой строке найдено " << counter << "  предложений\n";
-		cout << "За время: " << le_time - ls_time << endl;
-	// end linear block
-	}
+        cout << counter << " sentences found in the entered string\n";
+        counter = 0;
+		cout << "Time: " << le_time - ls_time << endl;
+    // end linear block
 
-	// parallel block
-	start_time = MPI_Wtime();
-	
-	MPI_Bcast(str, n, MPI_CHAR, 0, MPI_COMM_WORLD);
-	
-	int size = n / ProcNum;
-	int start = size * ProcRank;
-	int end = size * (ProcRank + 1);
-	
-	for(int i = start; i < end; i++)
-	{
-		if(searchDot(str[i]))
-		{
-			if(!searchDot(str[i - 1]))
-			{
-				my_counter++;
-			}
-		}
-	}
-	
-	MPI_Reduce(&my_counter, &counter, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    // parallel block
+        start_time = MPI_Wtime();
+        CounterVec = new int[ProcNum];
+        for(int i = 1; i < ProcNum; i++)
+            MPI_Send(str + n / ProcNum * i, n / ProcNum, MPI_CHAR, i, 0, MPI_COMM_WORLD);
+		
+        for (int i = 1; i < n / ProcNum; i++)
+        {
+            if (searchDot(str[i]))
+            {
+                if (!searchDot(str[i - 1]))
+                {
+                    my_counter++;
+                }
+            }
+        }
+        CounterVec[0] = my_counter;
 
-	if(ProcRank == 0)
-	{
-		remainderDiv = n % size;
-		if(remainderDiv)
-		{
-			for(int i = 1; i < remainderDiv; i++)
-			{
-				if(searchDot(str[n - remainderDiv + i]))
-				{
-					if(!searchDot(str[n - remainderDiv + i - 1]))
-					{
-						counter++;
-					}
-				}
-			}
-		}
-	
-	// parallel results
-		end_time = MPI_Wtime();
-		cout << "\n PARALLEL \n";
-		cout << "В введённой строке найдено " << counter << " предложений\n";
-		cout << "За время: " << end_time - start_time << endl;;
-	}
+        for(int i = 1; i < ProcNum; i++)
+            MPI_Recv(&CounterVec[i], 1, MPI_INT, i, 0, MPI_COMM_WORLD, &status);
+    
+    // check the remainder of string
+        remainderDiv = n % ProcNum;
+        if (remainderDiv)
+        {
+            for (int i = 0; i < remainderDiv; i++)
+            {
+                if (searchDot(str[n - remainderDiv + i]))
+                {
+                    if (!searchDot(str[n - remainderDiv + i - 1]))
+                    {
+                        counter++;
+                    }
+                }
+            }
+        }
+        CounterVec[0] += counter;
+
+    // parallel results
+        counter = 0;
+        for(int i = 0; i < ProcNum; i++)
+        {
+            counter += CounterVec[i];
+            // cout << CounterVec[i] << endl;
+        }
+        delete[]CounterVec;
+        end_time = MPI_Wtime();
+        cout << "\n PARALLEL \n";
+        cout << counter << " sentences found in the entered string\n";
+        cout << "Time: " << end_time - start_time << endl << endl;
+    } else {
+        my_str = new char[n / ProcNum];
+        MPI_Recv(my_str, n / ProcNum, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
+        for (int i = 0; i < n / ProcNum; i++)
+        {
+            if (searchDot(my_str[i]))
+            {
+                if (!searchDot(my_str[i - 1]))
+                {
+                    my_counter++;
+                }
+            }
+        }
+        delete[]my_str;
+        MPI_Send(&my_counter, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+    }
 	MPI_Finalize();
+	// cin.ignore();
 	// end parallel block
-
+    delete[]str;
+    
 	return 0;
 }
